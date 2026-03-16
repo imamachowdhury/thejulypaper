c09bc175a44cc58d8f908b2e039140e111835496
im4m <imamahmed007@gmail.com>
2026-03-16 17:18:03 +0600

    fix: move toBangla helper to AppServiceProvider to avoid undefined function error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!function_exists('toBangla')) {
+            function toBangla($number) {
+                if ($number === null) return '';
+                $search = ['0','1','2','3','4','5','6','7','8','9'];
+                $replace = ['০','১','২','৩','৪','৫','৬','৭','৮','৯'];
+                return str_replace($search, $replace, $number);
+            }
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,6 @@
 @php 
     use Carbon\Carbon;
     Carbon::setLocale('bn');
-
-    if (!function_exists('toBangla')) {
-        function toBangla($number) {
-            $search = ['0','1','2','3','4','5','6','7','8','9'];
-            $replace = ['০','১','২','৩','৪','৫','৬','৭','৮','৯'];
-            return str_replace($search, $replace, $number);
-        }
-    }
 @endphp
 <!DOCTYPE html>
 <html lang="bn" class="scroll-smooth">
